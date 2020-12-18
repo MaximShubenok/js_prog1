85a5f5411969ce92c627f98edd5783eea4e929b6
MaximShubenok <maxim.shubenok07@yandex.ru>
2020-12-19 02:36:59 +0300

    Empty lines

--- js/script.js
+++ js/script.js
@@ -24,7 +24,14 @@ for (let i = 0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ""),
           b = prompt("На сколько оцените его?", "");
 
-        personalMovieDB.movies[a] = b;
+          if (a != null && b != null && a!= '' && b !='' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+          } else {
+              i--;
+              console.log('Error');
+          }
+
 }
 
 console.log(personalMovieDB);
