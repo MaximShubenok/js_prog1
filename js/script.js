7609225e758a0de66a5acb37869ce297d3f7cb31
MaximShubenok <maxim.shubenok07@yandex.ru>
2020-12-19 00:17:01 +0300

    personal movie DB

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-var numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+var numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
